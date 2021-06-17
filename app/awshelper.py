8c20e60afd1f7acb752c56873503291e30dc1834
mike <mikemihalchik@gmail.com>
2021-06-17 10:30:24 -0400

    s

--- app/awshelper.py
+++ app/awshelper.py
@@ -4,19 +4,13 @@ import uuid
 
 BUCKET_NAME = os.environ.get("S3_BUCKET")
 S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
-# ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif", "vp8", "opus"}
+
 
 s3 = boto3.client(
    "s3",
    aws_access_key_id=os.environ.get("S3_KEY"),
    aws_secret_access_key=os.environ.get("S3_SECRET")
 )
-
-
-# def allowed_file(filename):
-#     return "." in filename and \
-#            filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 def get_unique_filename(filename):
     ext = filename.rsplit(".", 1)[1].lower()
