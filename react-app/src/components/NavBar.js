1c8114aba73351bbd3b92d434fe27c5802662911
mike <mikemihalchik@gmail.com>
2021-06-28 12:03:30 -0400

    working on responsiveness

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -14,7 +14,6 @@ const NavBar = () => {
   const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 })
   const isBigScreen = useMediaQuery({ minDeviceWidth: 1824 })
   const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
-  const isTabletOrMobileDevice = useMediaQuery({ maxDeviceWidth: 1224 })
   const isPortrait = useMediaQuery({ orientation: 'portrait' })
   const isRetina = useMediaQuery({ minResolution: '2dppx' })
 
@@ -79,6 +78,61 @@ const NavBar = () => {
         </li>
         }
         </div>
+      </ul>}
+      {(isTabletOrMobile || isPortrait || isRetina) && <ul className="navbar-list">
+        <li className='home navlink'>
+          <NavLink className='navlink' to="/" exact={true} activeClassName='active'>
+            Home
+          </NavLink>
+        </li>
+        <ul className='mobile-menu'>
+        <li className='info navlink'>
+          <NavLink className='navlink' to="/info" exact={true} activeClassName='active'>
+            Interview Guide
+          </NavLink>
+        </li>
+        <li className='info navlink'>
+          <NavLink className='navlink' to="/about" exact={true} activeClassName='active'>
+            About The Developer
+          </NavLink>
+        </li>
+
+
+       {user && <li className='interview navlink'>
+
+          <NavLink className='navlink' to="/interview" exact={true} activeClassName='active'>
+           Start an Interview
+          </NavLink>
+        </li>}
+
+       {user && <li className='interview navlink'>
+       <NavLink className='navlink' to="/liveinterview" exact={true} activeClassName='active'>
+           Live Interview
+          </NavLink>
+        </li>}
+
+       {user && <li className='feedback navlink'>
+          <NavLink className='navlink' to="/feedback" exact={true} activeClassName='active'>
+           View Feedback
+          </NavLink>
+        </li>}
+        <div className='user-buttons'>
+        { !user && <li className='login navlink'>
+          <LoginModal />
+          </li>}
+        {!user && <li className='signup navlink'>
+         <SignupModal />
+        </li>}
+        {!user && <li className='signup navlink'>
+         <button className='button' onClick={demoHandler}>Demo</button>
+        </li>}
+        { user &&
+          <li className='logout navlink'>
+          <LogoutButton />
+        </li>
+        }
+        </div>
+        </ul>
       </ul>}
     </nav>
   );
